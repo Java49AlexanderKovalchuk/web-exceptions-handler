2b281aa8ebe881e1af32230762c6c09538fa8708
AlexKovalchuk <kovalchuk111@gmail.com>
2023-11-13 12:28:11 +0200

    HW #58 solution from classwork

--- src/main/java/telran/exeptions/GlobalExeptionsHandler.java
+++ src/main/java/telran/exeptions/GlobalExeptionsHandler.java
@@ -1,7 +1,12 @@
 package telran.exeptions;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,27 +17,29 @@ import lombok.extern.slf4j.Slf4j;
 @ControllerAdvice
 @Slf4j
 public class GlobalExeptionsHandler {
+	
+	@ExceptionHandler({MethodArgumentNotValidException.class}) 
+		ResponseEntity<String> handlerMethodArgument(MethodArgumentNotValidException e) {
+			List<ObjectError> errors = e.getAllErrors();
+			String body = errors.stream().map(err -> err.getDefaultMessage())
+					.collect(Collectors.joining(";"));
+			return errorResponse(body, HttpStatus.BAD_REQUEST);
+	}
+	
+	private ResponseEntity<String> errorResponse(String body, HttpStatus status) {
+		log.error(body);
+		return new ResponseEntity<String>(body, status);
+	}
+
 	@ExceptionHandler({ IllegalStateException.class })
 	ResponseEntity<String> badRequest(IllegalStateException e) {
 		String message = e.getMessage();
-		log.error("bad request. {}", message);
-		return new ResponseEntity<String>(message, HttpStatus.BAD_REQUEST);   
+		return errorResponse(message, HttpStatus.BAD_REQUEST);   
 	}
 	@ExceptionHandler({NotFoundExeption.class})
-	ResponseEntity<String> notFound(NotFoundExeption e) {
+	ResponseEntity<String> notFound(NotFoundExeption e) { 
 		String message = e.getMessage();
-		log.error("not found. {}", message);
-		return new ResponseEntity<String>(message, HttpStatus.NOT_FOUND);
-	}
-	
-	@ExceptionHandler({MethodArgumentNotValidException.class})
-	ResponseEntity<String> noValidArg(MethodArgumentNotValidException e) {
-		String message = e.getFieldError().getDefaultMessage();
-		String argField = e.getFieldError().getField();
-		log.error("Method noValidArg: exeption {} argument {}", argField, message);
-		return new ResponseEntity<String>(argField + " " + message, HttpStatus.BAD_REQUEST);
-	}
-	
-	
+		return errorResponse(message, HttpStatus.NOT_FOUND);   
+	 }
 	
 }
