e7ca1741e5cab89b191d16712bcecda194641696
AlexKovalchuk <kovalchuk111@gmail.com>
2023-11-13 16:53:22 +0200

    improve GlobalExceptionHandler

--- src/main/java/telran/exeptions/GlobalExeptionsHandler.java
+++ src/main/java/telran/exeptions/GlobalExeptionsHandler.java
@@ -1,11 +1,10 @@
 package telran.exeptions;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,12 +17,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GlobalExeptionsHandler {
 	
-	@ExceptionHandler({MethodArgumentNotValidException.class}) 
-		ResponseEntity<String> handlerMethodArgument(MethodArgumentNotValidException e) {
-			List<ObjectError> errors = e.getAllErrors();
-			String body = errors.stream().map(err -> err.getDefaultMessage())
-					.collect(Collectors.joining(";"));
-			return errorResponse(body, HttpStatus.BAD_REQUEST);
+	@ExceptionHandler(MethodArgumentNotValidException.class) 
+	ResponseEntity<String> handlerMethodArgument(MethodArgumentNotValidException e) {
+		List<ObjectError> errors = e.getAllErrors();
+		String body = errors.stream().map(err -> err.getDefaultMessage())
+				.collect(Collectors.joining(";"));
+		return errorResponse(body, HttpStatus.BAD_REQUEST);
 	}
 	
 	private ResponseEntity<String> errorResponse(String body, HttpStatus status) {
@@ -31,8 +30,8 @@ public class GlobalExeptionsHandler {
 		return new ResponseEntity<String>(body, status);
 	}
 
-	@ExceptionHandler({ IllegalStateException.class })
-	ResponseEntity<String> badRequest(IllegalStateException e) {
+	@ExceptionHandler({ IllegalStateException.class, HttpMessageNotReadableException.class })
+	ResponseEntity<String> badRequest(Exception e) {
 		String message = e.getMessage();
 		return errorResponse(message, HttpStatus.BAD_REQUEST);   
 	}
